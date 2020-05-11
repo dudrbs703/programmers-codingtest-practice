96bea8f8a5844ae7c3e23c8544fd71ac844cbc05
cm4ng <dudrbs703@naver.com>
2020-05-11 12:53:23 +0900

    Update ListOfPhoneNumber.java
     - 코드 수정

--- Level2/ListOfPhoneNumber.java
+++ Level2/ListOfPhoneNumber.java
@@ -23,8 +23,10 @@ public class ListOfPhoneNumber
     public static boolean solution(String []phone_book)
     {
         boolean answer = true;
-        String prefix = phone_book[0];
+        
         /*
+        String prefix = phone_book[0];
+        
         Map<String, String> map = new HashMap<>();
         for(int i=1; i<phone_book.length; i++) map.put(phone_book[i], "phone");
 
@@ -33,14 +35,37 @@ public class ListOfPhoneNumber
             if(entry.getKey().contains(prefix)) return false;
         }*/
 
+        /*
         for(int i=0; i<phone_book.length-1;i++)
         {
             for(int j=i+1; j<phone_book.length; j++)
             {
-                if(phone_book[i].startsWith(phone_book[j])) return false;
-                if(phone_book[j].startsWith(phone_book[i])) return false;
+                if(phone_book[i].startsWith(phone_book[j]))
+                { 
+                    System.out.println(i+" 1 "+phone_book[i]+" "+phone_book[j]);
+                    return false;
+                };
+                if(phone_book[j].startsWith(phone_book[i])){
+                    System.out.println(i+" 2 "+phone_book[i]+" "+phone_book[j]);
+                    return false;
+                } 
             }
+            
 
+        } 
+
+        */
+
+
+        Arrays.sort(phone_book);
+
+        for(int i=0; i<phone_book.length-1; i++)
+        {
+            if(phone_book[i+1].startsWith(phone_book[i]))
+            {
+                answer = false;
+                break;
+            }
         }
         return answer;
     }
